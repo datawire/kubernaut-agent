a17d3187a06fd0ce2e08423ac985872db818732f
Philip Lombardi <plombardi@datawire.io>
2018-02-04 17:21:52 -0500

    Very rough sketch impl. of the cluster agent

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 The Kubernaut Agent is a small program that communicates with the Kubernaut Controller about cluster availability and claims.
 
+# Cluster Agent Protocol v1
+
+The Kubernaut Cluster Agent Protocol v1 (CAP/1.0) is a JSON payload bi-directional WebSocket protocol with a handful of message types.
+
 # License
 
 Released under Apache 2.0. Please read [LICENSE](LICENSE) for details.
--- kubernaut/agent.py
+++ kubernaut/agent.py
@@ -1,47 +1,34 @@
 import asyncio
 import click
+import json
 
 from autobahn.asyncio.websocket import WebSocketClientProtocol, WebSocketClientFactory
 
 
+CONTROLLER_HOST = "kubernaut.io"
+CONTROLLER_PORT = 443
+CONTROLLER_PROTO = "wss"
+
+
 @click.command()
 @click.option(
-    "--controller-host",
-    envvar="KUBERNAUT_CONTROLLER_HOST",
-    help="Configure remote Kubernaut Controller host (fmt: host:port)",
+    "--controller-endpoint",
+    envvar="KUBERNAUT_CONTROLLER_ENDPOINT",
+    help="Configure remote Kubernaut Controller endpoint",
+    default="wss://kubernaut.io/cap-v1",
     type=str
 )
-def agent(controller_host):
+@click.argument(
+    "cluster_id",
+    envvar="KUBERNAUT_CLUSTER_ID",
+)
+@click.argument(
+    "kubeconfig_file",
+    envvar="KUBERNAUT_CLUSTER_KUBECONFIG_FILE",
+)
+def agent(controller_endpoint, cluster_id, kubeconfig_file):
     print("Starting Kubernaut Agent...")
-    start_agent()
-
-
-class AgentProtocol(WebSocketClientProtocol):
-
-    def onConnect(self, response):
-        print("Server connected: {0}".format(response.peer))
-
-    def onOpen(self):
-        print("WebSocket connection open.")
-
-        def hello():
-            self.sendMessage(u"Hello, world!".encode('utf8'))
-            self.factory.loop.call_later(1, hello)
-
-        hello()
-
-    def onMessage(self, payload, is_binary):
-        if is_binary:
-            print("Binary message received: {0} bytes".format(len(payload)))
-        else:
-            print("Text message received: {0}".format(payload.decode('utf8')))
-
-    def onClose(self, was_clean, code, reason):
-        print("WebSocket connection closed: {0}".format(reason))
-
-
-def start_agent():
-    factory = WebSocketClientFactory("ws://127.0.0.1:7000")
+    factory = WebSocketClientFactory("ws://127.0.0.1:7000/cap-v1")
     factory.protocol = AgentProtocol
 
     loop = asyncio.get_event_loop()
@@ -49,6 +36,67 @@ def start_agent():
     loop.run_until_complete(conn)
     loop.run_forever()
     loop.close()
+
+
+class AgentProtocol(WebSocketClientProtocol):
+
+    def __init__(self, tenant_id, pool_name, kubeconfig_file):
+        super().__init__()
+        self.state = "unregistered"
+
+        self.tenant_id = tenant_id
+        self.pool_name = pool_name
+        self.kubeconfig_file = kubeconfig_file
+        self.cluster_id = None
+
+    def onConnect(self, response):
+        print("Connected to remote 'address = {0}'".format(response.peer))
+
+    def onOpen(self):
+        self._register()
+
+    def onMessage(self, payload, is_binary):
+        data = json.loads(payload, encoding="utf-8")
+        self._handle_received_message(data)
+
+    def onClose(self, was_clean, code, reason):
+        self.state = "unregistered"
+
+    def _register(self):
+        if not self.state == "claimed":
+            msg = {
+                "type": "registration",
+                "cluster": {
+                    "poolName": self.pool_name,
+                    "kubeconfig": self.kubeconfig_file
+                }
+            }
+
+            self.sendMessage(json.dumps(msg))
+
+        self.factory.loop.call_later(1, self._register)
+
+    def _handle_registration_accepted(self):
+        self.state = "registration:accepted"
+
+    def _handle_claim(self):
+        self.state = "claimed"
+
+    def _handle_received_message(self, msg):
+        msg_types = {
+            "registration:accepted": self._handle_registration_accepted,
+            "claimed": self._handle_claim
+        }
+
+        try:
+            msg_types[msg["type"].tolower()](msg)
+        except KeyError:
+            pass
+
+
+def load_kubeconfig(kubeconfig_path):
+    from pathlib import Path
+    return Path(kubeconfig_path).read_text(encoding="utf-8")
 
 
 if __name__ == "__main__":
