4adea4828885eed9b6ad30303f2c0aa75009565b
plombardi <plombardi89@gmail.com>
2018-02-08 13:23:48 -0500

    Discover Namespace UID as mechanism for cluster identification

---
+++ kubernaut/kubernetes.py
@@ -0,0 +1,50 @@
+import typing
+import json
+import os
+
+from pathlib import Path
+from subprocess import run, STDOUT, PIPE
+from typing import List, Mapping, Tuple
+
+
+def find_kubectl(search: List[str] = None) -> str:
+    for p in search:
+        p = Path(p)
+        p = p / "kubectl"
+        if p.is_file() and os.access(str(p), os.X_OK):
+            return str(p)
+
+    raise ValueError("Unable to find `kubectl` program on system")
+
+
+kubectl_exec = find_kubectl(["/bin", "/usr/local/bin"])
+
+
+def discover_cluster_id(namespace: str = "kube-system",
+                        kubeconfig: str = os.path.expanduser("~/.kube/config")) -> str:
+
+    """Gets a Kubernetes cluster ID.
+
+    Kubernetes as of v1.9.x does not have an official concept of cluster
+    identity, however, the agent considers the UID of a namespace argument to be
+    the canonical ID. Selection of the namespace is important however.
+
+    :argument namespace the namespace to retrieve the UID from.
+    :argument kubeconfig the path to the kubeconfig file to use.
+
+    :return: the given namespaces UID acting as cluster ID.
+    """
+
+    (status, output) = kubectl(args=["get", "namespace", namespace, "--output=json"],
+                               env={"KUBECONFIG": os.path.expanduser(kubeconfig)})
+    if status == 0:
+        return json.loads(output)["metadata"]["uid"]
+    else:
+        raise ValueError("Get namespace 'name = {}' failed 'exitcode = {}'", namespace, status)
+
+
+def kubectl(args: List[str], env: Mapping[str, str] = None) -> Tuple[int, str]:
+    args.insert(0, kubectl_exec)
+    completed = run(args, shell=False, stdout=PIPE, stderr=STDOUT, env=env)
+
+    return completed.returncode, completed.stdout
