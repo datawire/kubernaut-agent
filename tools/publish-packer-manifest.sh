1f40b84a93ead8f635939c2108b480bbf86b6e8c
Philip Lombardi <plombardi@datawire.io>
2018-07-03 14:31:59 -0400

    It helps to actually push the deploy script...

---
+++ tools/publish-packer-manifest.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o verbose
+
+aws s3api put-object \
+    --bucket datawire-static-files \
+    --key kubernautlet/packer-manifest.json \
+    --body packer/packer-manifest.json
