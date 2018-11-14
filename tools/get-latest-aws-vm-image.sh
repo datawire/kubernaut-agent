553e16446fef2b5adf11a6a5d5ba77434fd42708
plombardi <plombardi89@gmail.com>
2018-11-14 12:01:43 -0500

    get rid of end=

--- tools/get-latest-aws-vm-image.sh
+++ tools/get-latest-aws-vm-image.sh
@@ -7,4 +7,4 @@ set -o verbose
 wget -O /tmp/packer-manifest.json \
     https://s3.amazonaws.com/datawire-static-files/kubernautlet/packer-manifest.json
 
-cat /tmp/packer-manifest.json | python -c 'import json,sys; obj=json.load(sys.stdin); print([x for x in obj["builds"] if x["builder_type"] == "amazon-ebs"][0]["artifact_id"].split(":")[1], end="");'
+cat /tmp/packer-manifest.json | python -c 'import json,sys; obj=json.load(sys.stdin); print([x for x in obj["builds"] if x["builder_type"] == "amazon-ebs"][0]["artifact_id"].split(":")[1]);'
